6603702723787236ad4e3468861c23e4d5cc7568
Kunmun <kunmun@aospa.co>
2022-12-02 18:11:27 +0530

    corfur: Makefile: Drop updatable apex
    Change-Id: I237559b70ed66a631606f802db6eee2e499d5465

--- device.mk
+++ device.mk
@@ -22,9 +22,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
