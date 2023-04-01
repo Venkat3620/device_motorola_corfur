c6b70b51340c37b5b9e37f63586e92bf55eb7357
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2023-04-02 04:06:51 +0530

    corfur: update cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,18 +56,23 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 BOARD_BOOT_HEADER_VERSION := 3
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
+    androidboot.console=ttyMSM0 \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
     androidboot.usbcontroller=4e00000.dwc3 \
+    androidboot.hab.cid=50 \
+    androidboot.hab.csv=11 \
+    androidboot.hab.product=corfur \
+    console=ttyMSM0,115200n8 \
     cgroup.memory=nokmem,nosocket \
-    loop.max_part=7 \
+    earlycon=msm_geni_serial,0x04C8C000 \
+    firmware_class.path=/vendor/firmware_mnt/image \
+    iptable_raw.raw_before_defrag=1 ip6table_raw.raw_before_defrag=1 \
     lpm_levels.sleep_disabled=1 \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
     msm_rtb.filter=0x237 \
-    pcie_ports=compat \
     service_locator.enable=1 \
-    swiotlb=0 \
-    ip6table_raw.raw_before_defrag=1 \
-    iptable_raw.raw_before_defrag=1
+    swiotlb=0 loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
