e1717a1f56754d0c842e6225cbb7e5e0e3669c84
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2023-04-04 01:30:29 +0530

    corfur: selinux neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,6 +145,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
