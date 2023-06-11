328a36a22dc37ea6c8ee13bd5fab91e33e4500c4
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2023-06-11 13:37:27 +0530

    corfur: fix build err

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,7 +142,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Sepolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 SELINUX_IGNORE_NEVERALLOWS := true
