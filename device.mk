f1c34aca4b93705d89d8b7def494635ef25e5238
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2023-06-29 22:26:13 +0530

    corfur: Import stock manifest

--- device.mk
+++ device.mk
@@ -191,8 +191,6 @@ PRODUCT_PACKAGES += \
 
 # Manifests
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/configs/vintf/corfur_vendor_framework_compatibility_matrix.xml
 
 # Moto Actions
 PRODUCT_PACKAGES += \
