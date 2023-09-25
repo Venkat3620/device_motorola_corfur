ab0b12f7e8dc4eb6669329998cae50399d5c9a95
Venkat Yadlapati <venkat.yadlapati@hotmail.com>
2023-09-25 19:27:22 +0530

    corfur: Add lineage overlays

--- device.mk
+++ device.mk
@@ -90,6 +90,12 @@ PRODUCT_PACKAGES += \
     MotoActions \
     MotoCommonOverlay
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
