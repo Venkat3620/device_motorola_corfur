6603a2a7155327662141254c160f5e3ba2b50101
Cosmin Tanislav <demonsingur@gmail.com>
2024-02-28 16:15:00 +0530

    corfur: do not build useless packages
    Change-Id: I72076113083308e26573289923f5d3c00a8a6b44

--- device.mk
+++ device.mk
@@ -256,9 +256,7 @@ $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
-    IPACM_cfg.xml \
-    libipanat \
-    liboffloadhal
+    IPACM_cfg.xml
 
 # IPC router config
 PRODUCT_COPY_FILES += \
@@ -399,8 +397,7 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
-    librmnetctl \
-    libxml2
+    librmnetctl
 
 # Sensors
 PRODUCT_PACKAGES += \
